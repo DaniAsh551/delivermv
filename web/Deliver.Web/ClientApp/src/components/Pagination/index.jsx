f2930bd93d93c8c55a3f9e0836eeee71cd3b1dae
DaniAsh551 <d4n1.551@gmail.com>
2020-04-22 15:18:25 +0500

    fix: pagination and identity

--- web/Deliver.Web/ClientApp/src/components/Pagination/index.jsx
+++ web/Deliver.Web/ClientApp/src/components/Pagination/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import API from '../../api';
+import ReactPaginate from 'react-paginate';
 
 export default function Pagination({ paging, onPage, allowGranularControl = false }) {
 
@@ -59,44 +60,23 @@ export default function Pagination({ paging, onPage, allowGranularControl = fals
 
     return (
         <div className="text-center" style={{width:'100%'}}>
-            <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                    
-                    {
-                        hasPrev && 
-                        <>
-                            <li className="page-item" style={{cursor:'pointer'}}>
-                                <a className="page-link" aria-label="Previous" onClick={() => onPage(1)}>
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span className="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            {
-                                Array(getLower(page - 1, 2)).fill(null).map((_,i) => (
-                                    <li className="page-item" style={{cursor:'pointer'}}><a className="page-link" onClick={() => onPage(page - (i + 1))}>{page - (i + 1)}</a></li>
-                                ))
-                            }
-                        </>
-                    }
-                    <li className="page-item"><span className="page-link">{page}</span></li>
-                    {
-                        hasAfter &&
-                        <>
-                            {
-                                Array(getLower(totalPages - 1, 2)).fill(null).map((_,i) => (
-                                    <li className="page-item" style={{cursor:'pointer'}}><a className="page-link" onClick={() => onPage(page + (i + 1))}>{page + (i + 1)}</a></li>
-                                ))
-                            }
-                            <li className="page-item" style={{cursor:'pointer'}}>
-                                <a className="page-link" aria-label="Next" onClick={() => onPage(totalPages)}>
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span className="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </>
-                    }
-                    
-                </ul>
+            <nav>
+                <ReactPaginate
+                onPageChange={p => console.log(p.selected + 1)}
+                containerClassName="pagination"
+                pageCount={totalPages}
+                pageRangeDisplayed={2}
+                marginPagesDisplayed={2}
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                activeClassName="page-item active"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                previousLabel={'<'}
+                initialPage={page}
+                nextLabel={'>'} />
             </nav>
         </div>
     );
