61e022a5794a180218854774a304065b2567a57c
DaniAsh551 <d4n1.551@gmail.com>
2020-04-22 12:28:39 +0500

    feat: Service Worker

--- web/Deliver.Web/ClientApp/src/registerServiceWorker.js
+++ web/Deliver.Web/ClientApp/src/registerServiceWorker.js
@@ -106,14 +106,3 @@ export function unregister () {
     });
   }
 }
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      cache.put(event.request, response.clone());
-      return response;
-    }).catch(_ => {
-      return caches.match(event.request);
-    })
-  );
- });
